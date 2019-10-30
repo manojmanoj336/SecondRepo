cb01c756e1df7baec13f8cecc3eb8bdd5f26c50c
manoj kumar <pc1@Manoj_Kumar>
2019-10-30 22:11:13 +0530

    just for chdeck

--- discovery-server/src/main/java/com/mypack/DiscoveryServerApp.java
+++ discovery-server/src/main/java/com/mypack/DiscoveryServerApp.java
@@ -7,6 +7,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApp {
+	/*public void grtDa()
+	{
+		
+	}*/
+	
   public static void main(String[] args) {
     SpringApplication.run(DiscoveryServerApp.class, args);
   }
